f419cd4b222c683879c42eb5a029a960620aed70
mattak <mattak.me@gmail.com>
2016-06-09 23:50:43 +0900

    issues/6 feat(Binder): TextBinder added
    It handle UnityEngine.UI.Text

---
+++ Assets/Unidux/Binder/TextBinder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+
+namespace Unidux.Binder
+{
+    public static class TextBinder
+    {
+        public delegate string StringBindDelegate<T>(T state) where T : StateBase, new();
+
+        public static void BindTo<T>(this Text text, Store<T> store, StringBindDelegate<T> caller) where T : StateBase, new()
+        {
+            text.gameObject.AddTo<T>(store, state => { text.text = caller(state); });
+        }
+    }
+}
