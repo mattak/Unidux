383b68606b36dac214249dd9056d2cceb6c77df1
mattak <mattak.me@gmail.com>
2016-06-09 23:53:33 +0900

    issues/6 feat(Binder): Binder for changing active state

---
+++ Assets/Unidux/Binder/ActiveBinder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Unidux.Binder
+{
+    public static class ActiveBinder
+    {
+        public delegate bool BoolBindDelegate<T>(T state) where T : StateBase, new();
+
+        public static void BindActiveTo<T>(this GameObject gameObject, Store<T> store, BoolBindDelegate<T> caller)
+            where T : StateBase, new()
+        {
+            gameObject.AddSustainTo(store, state => { gameObject.SetActive(caller(state)); });
+        }
+    }
+}
