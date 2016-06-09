746f44715032aff8b837776f695f3b1f91f2d1b3
mattak <mattak.me@gmail.com>
2016-06-09 23:51:47 +0900

    issues/6 feat(Binder): ImageBinder added
    It handle UnityEngine.UI.Image.

---
+++ Assets/Unidux/Binder/ImageBinder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unidux.Binder
+{
+    public static class ImageBinder
+    {
+        public delegate Sprite SpriteBindDelegate<T>(T state) where T : StateBase, new();
+        public delegate string SpritePathBindDelegate<T>(T state) where T : StateBase, new();
+
+        public static void BindTo<T>(this Image image, Store<T> store, SpriteBindDelegate<T> caller) where T : StateBase, new()
+        {
+            image.gameObject.AddTo(store, state => { image.sprite = caller(state); });
+        }
+
+        public static void BindPathTo<T>(this Image image, Store<T> store, SpritePathBindDelegate<T> caller) where T : StateBase, new()
+        {
+            image.gameObject.AddTo(store, state => { image.sprite = Resources.Load<Sprite>(caller(state)); });
+        }
+    }
+}
