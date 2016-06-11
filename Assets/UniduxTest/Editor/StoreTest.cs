e68fe27556784de52e8e13f164723500525f75fb
mattak <mattak.me@gmail.com>
2016-06-11 11:19:36 +0900

    issues/10 test(Store): unit test for Render, Reducer subscribe

---
+++ Assets/UniduxTest/Editor/StoreTest.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using Unidux;
+
+public class StoreTest
+{
+    [Test]
+    public void RenderSubscribeTest()
+    {
+        var store = new Store<State>();
+        var render = new SampleRender();
+        store.RenderEvent += render.Render;
+
+        store.Dispatch(new Action());
+        store.ForceUpdate();
+        Assert.AreEqual(1, render.Count);
+
+        store.Dispatch(new Action());
+        store.ForceUpdate();
+        Assert.AreEqual(2, render.Count);
+
+        store.RenderEvent -= render.Render;
+
+        store.Dispatch(new Action());
+        store.ForceUpdate();
+        Assert.AreEqual(2, render.Count);
+    }
+
+    [Test]
+    public void ReducerSubscribeTest()
+    {
+        var store = new Store<State>();
+        var reducer = new SampleReducer();
+        store.AddReducer<Action>(reducer.Reduce);
+
+        store.Dispatch(new Action());
+        Assert.AreEqual(1, reducer.Count);
+
+        store.Dispatch(new Action());
+        Assert.AreEqual(2, reducer.Count);
+
+        store.RemoveReducer<Action>(reducer.Reduce);
+        store.Dispatch(new Action());
+        Assert.AreEqual(2, reducer.Count);
+    }
+
+    class State : StateBase
+    {
+    }
+
+    class Action
+    {
+    }
+
+    class SampleRender
+    {
+        public int Count = 0;
+        public void Render(State state)
+        {
+            Count++;
+        }
+    }
+
+    class SampleReducer
+    {
+        public int Count = 0;
+
+        public State Reduce(State state, Action action)
+        {
+            Count++;
+            return state;
+        }
+    }
+}
