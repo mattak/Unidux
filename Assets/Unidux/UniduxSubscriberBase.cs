e71208af1de394e5ea7d6b829ada1edd0bc53125
mattak <mattak.me@gmail.com>
2016-06-30 11:44:38 +0900

    issues/31 feat(Unidux): UniduxSubscriber accept multiple AddTo function

--- Assets/Unidux/UniduxSubscriberBase.cs
+++ Assets/Unidux/UniduxSubscriberBase.cs
@@ -1,43 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Unidux
 {
     public class UniduxSubscriberBase : MonoBehaviour, IUniduxSubscriber
     {
-        protected Action<bool> _renderSubscriber = (enable) => { };
-        protected Action<bool> _reduceSubscriber = (enable) => { };
+        private readonly Dictionary<int, Action<bool>> _renderSubscriberMap = new Dictionary<int, Action<bool>>();
+        private readonly Dictionary<int, Action<bool>> _reduceSubscriberMap = new Dictionary<int, Action<bool>>();
 
         public void AddRenderTo<S>(Store<S> store, Render<S> render) where S : StateBase, new()
         {
-            _renderSubscriber = (enable) =>
+            Action<bool> renderSubscriber = null;
+            int key = render.GetHashCode();
+
+            if (_renderSubscriberMap.ContainsKey(key))
             {
-                if (enable)
+                renderSubscriber = _renderSubscriberMap[key];
+            }
+            else
+            {
+                renderSubscriber = (enable) =>
                 {
-                    store.RenderEvent += render;
-                }
-                else
-                {
-                    store.RenderEvent -= render;
-                }
-            };
-            _renderSubscriber(true);
+                    if (enable)
+                    {
+                        store.RenderEvent += render;
+                    }
+                    else
+                    {
+                        store.RenderEvent -= render;
+                    }
+                };
+            }
+            renderSubscriber(true);
+
+            _renderSubscriberMap[key] = renderSubscriber;
         }
 
-        public void AddReducerTo<S, A>(Store<S> store, Reducer<S, A> render) where S : StateBase, new()
+        public void AddReducerTo<S, A>(Store<S> store, Reducer<S, A> reducer) where S : StateBase, new()
         {
-            _reduceSubscriber = (enable) =>
+            Action<bool> reduceSubscriber = null;
+            int key = reducer.GetHashCode();
+
+            if (_reduceSubscriberMap.ContainsKey(key))
             {
-                if (enable)
+                reduceSubscriber = _reduceSubscriberMap[key];
+            }
+            else
+            {
+                reduceSubscriber = (enable) =>
                 {
-                    store.AddReducer(render);
-                }
-                else
-                {
-                    store.RemoveReducer(render);
-                }
-            };
-            _reduceSubscriber(true);
+                    if (enable)
+                    {
+                        store.AddReducer(reducer);
+                    }
+                    else
+                    {
+                        store.RemoveReducer(reducer);
+                    }
+                };
+            }
+            reduceSubscriber(true);
+
+            _reduceSubscriberMap[key] = reduceSubscriber;
+        }
+
+        protected void CallRenders(bool subscribe)
+        {
+            foreach (var caller in _renderSubscriberMap.Values)
+            {
+                caller(subscribe);
+            }
+        }
+
+        protected void CallReducers(bool subscribe)
+        {
+            foreach (var caller in _reduceSubscriberMap.Values)
+            {
+                caller(subscribe);
+            }
+        }
+
+        protected void DisposeRenders()
+        {
+            _renderSubscriberMap.Clear();
+        }
+
+        protected void DisposeReducers()
+        {
+            _reduceSubscriberMap.Clear();
         }
     }
 }
