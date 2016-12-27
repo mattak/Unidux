ac188fb80a5e770572b6430011d6bd5dbf7783f8
mattak <mattak.me@gmail.com>
2016-12-27 23:48:12 +0900

    refactor(Test): use namespace on Test

--- Assets/Plugins/Unidux/Test/Editor/StateCloneTest.cs
+++ Assets/Plugins/Unidux/Test/Editor/StateCloneTest.cs
@@ -1,57 +1,55 @@
-﻿using UnityEngine;
-using UnityEditor;
-using NUnit.Framework;
-using Unidux;
-
-public class StateCloneTest
+﻿using NUnit.Framework;
+namespace Unidux
 {
-    class Hoge<T> : IStateClone<T>
+    public class StateCloneTest
     {
-        private bool Flag = false;
-
-        public void SetFlag(bool flg)
+        class Hoge<T> : IStateClone<T>
         {
-            this.Flag = flg;
+            private bool Flag = false;
+
+            public void SetFlag(bool flg)
+            {
+                this.Flag = flg;
+            }
+
+            public bool GetFlag()
+            {
+                return this.Flag;
+            }
+
+            public T Clone()
+            {
+                return (T) MemberwiseClone();
+            }
         }
 
-        public bool GetFlag()
+        class HogeHoge : Hoge<HogeHoge>
         {
-            return this.Flag;
+            private int Count = 1;
+
+            public void SetCount(int count)
+            {
+                this.Count = count;
+            }
+
+            public int GetCount()
+            {
+                return this.Count;
+            }
         }
 
-        public T Clone()
+        [Test]
+        public void CloneTest()
         {
-            return (T)MemberwiseClone();
+            var hoge1 = new HogeHoge();
+            hoge1.SetFlag(true);
+            hoge1.SetCount(100);
+            Assert.AreEqual(true, hoge1.GetFlag());
+            Assert.AreEqual(100, hoge1.GetCount());
+
+            var hoge2 = hoge1.Clone();
+            Assert.AreEqual(true, hoge2.GetFlag());
+            Assert.AreEqual(100, hoge2.GetCount());
         }
-
-    }
-
-    class HogeHoge : Hoge<HogeHoge>
-    {
-        private int Count = 1;
-
-        public void SetCount(int count)
-        {
-            this.Count = count;
-        }
-
-        public int GetCount()
-        {
-            return this.Count;
-        }
-    }
-
-    [Test]
-    public void CloneTest()
-    {
-        var hoge1 = new HogeHoge();
-        hoge1.SetFlag(true);
-        hoge1.SetCount(100);
-        Assert.AreEqual(true, hoge1.GetFlag());
-        Assert.AreEqual(100, hoge1.GetCount());
-
-        var hoge2 = hoge1.Clone();
-        Assert.AreEqual(true, hoge2.GetFlag());
-        Assert.AreEqual(100, hoge2.GetCount());
     }
 }
--- Assets/Plugins/Unidux/Test/Editor/StoreTest.cs
+++ Assets/Plugins/Unidux/Test/Editor/StoreTest.cs
@@ -1,149 +1,142 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-using NUnit.Framework;
-using Unidux;
+﻿using NUnit.Framework;
 
-public class StoreTest
+namespace Unidux
 {
-    [Test]
-    public void RenderSubscribeTest()
+    public class StoreTest
     {
-        var store = new Store<State>(new State());
-        var render = new SampleRender();
-        store.AddRenderer(render.Render);
-
-        store.Dispatch(new Action());
-        store.ForceUpdate();
-        Assert.AreEqual(1, render.Count);
-
-        store.Dispatch(new Action());
-        store.ForceUpdate();
-        Assert.AreEqual(2, render.Count);
-
-        store.RemoveRenderer(render.Render);
-
-        store.Dispatch(new Action());
-        store.ForceUpdate();
-        Assert.AreEqual(2, render.Count);
-    }
-
-    [Test]
-    public void RenderMultipleSubscribeTest()
-    {
-        var store = new Store<State>(new State());
-        var count1 = 0;
-        var count2 = 0;
-        Unidux.Renderer<State> render1 = (State state) =>
+        [Test]
+        public void RenderSubscribeTest()
         {
-            count1++;
-        };
-        Unidux.Renderer<State> render2 = (State state) =>
-        {
-            count2++;
-        };
+            var store = new Store<State>(new State());
+            var render = new SampleRender();
+            store.AddRenderer(render.Render);
 
-        store.AddRenderer(render1);
-        store.AddRenderer(render2);
+            store.Dispatch(new Action());
+            store.ForceUpdate();
+            Assert.AreEqual(1, render.Count);
 
-        store.ForceUpdate();
-        Assert.AreEqual(1, count1);
-        Assert.AreEqual(1, count2);
+            store.Dispatch(new Action());
+            store.ForceUpdate();
+            Assert.AreEqual(2, render.Count);
 
-        store.RemoveRenderer(render1);
+            store.RemoveRenderer(render.Render);
 
-        store.ForceUpdate();
-        Assert.AreEqual(1, count1);
-        Assert.AreEqual(2, count2);
-
-        store.RemoveRenderer(render2);
-
-        store.ForceUpdate();
-        Assert.AreEqual(1, count1);
-        Assert.AreEqual(2, count2);
-    }
-
-    [Test]
-    public void ReducerSubscribeTest()
-    {
-        var store = new Store<State>(new State());
-        var reducer = new SampleReducer();
-        store.AddReducer<Action>(reducer.Reduce);
-
-        store.Dispatch(new Action());
-        Assert.AreEqual(1, reducer.Count);
-
-        store.Dispatch(new Action());
-        Assert.AreEqual(2, reducer.Count);
-
-        store.RemoveReducer<Action>(reducer.Reduce);
-        store.Dispatch(new Action());
-        Assert.AreEqual(2, reducer.Count);
-    }
-
-    [Test]
-    public void ResetStateChangedTest()
-    {
-        var store = new Store<State>(new State());
-        var count = 0;
-
-        store.AddRenderer((state) =>
-        {
-            count++;
-            Assert.IsTrue(state.Changed.IsStateChanged());
-        });
-
-        Assert.IsFalse(store.State.Changed.IsStateChanged());
-        store.State.Changed.SetStateChanged();
-
-        store.ForceUpdate();
-        Assert.IsFalse(store.State.Changed.IsStateChanged());
-        Assert.AreEqual(1, count);
-    }
-
-    class State : StateBase<State>
-    {
-        public ChangedState Changed { get; set; }
-
-        public State()
-        {
-            this.Changed = new ChangedState();
+            store.Dispatch(new Action());
+            store.ForceUpdate();
+            Assert.AreEqual(2, render.Count);
         }
 
-        public override State Clone()
+        [Test]
+        public void RenderMultipleSubscribeTest()
         {
-            var state = (State)MemberwiseClone();
-            state.Changed = state.Changed.Clone();
-            return state;
+            var store = new Store<State>(new State());
+            var count1 = 0;
+            var count2 = 0;
+            Unidux.Renderer<State> render1 = (State state) => { count1++; };
+            Unidux.Renderer<State> render2 = (State state) => { count2++; };
+
+            store.AddRenderer(render1);
+            store.AddRenderer(render2);
+
+            store.ForceUpdate();
+            Assert.AreEqual(1, count1);
+            Assert.AreEqual(1, count2);
+
+            store.RemoveRenderer(render1);
+
+            store.ForceUpdate();
+            Assert.AreEqual(1, count1);
+            Assert.AreEqual(2, count2);
+
+            store.RemoveRenderer(render2);
+
+            store.ForceUpdate();
+            Assert.AreEqual(1, count1);
+            Assert.AreEqual(2, count2);
         }
-    }
 
-    class ChangedState : StateElement<ChangedState>
-    {
-    }
-
-    class Action
-    {
-    }
-
-    class SampleRender
-    {
-        public int Count = 0;
-
-        public void Render(State state)
+        [Test]
+        public void ReducerSubscribeTest()
         {
-            Count++;
+            var store = new Store<State>(new State());
+            var reducer = new SampleReducer();
+            store.AddReducer<Action>(reducer.Reduce);
+
+            store.Dispatch(new Action());
+            Assert.AreEqual(1, reducer.Count);
+
+            store.Dispatch(new Action());
+            Assert.AreEqual(2, reducer.Count);
+
+            store.RemoveReducer<Action>(reducer.Reduce);
+            store.Dispatch(new Action());
+            Assert.AreEqual(2, reducer.Count);
         }
-    }
 
-    class SampleReducer
-    {
-        public int Count = 0;
-
-        public State Reduce(State state, Action action)
+        [Test]
+        public void ResetStateChangedTest()
         {
-            Count++;
-            return state;
+            var store = new Store<State>(new State());
+            var count = 0;
+
+            store.AddRenderer((state) =>
+            {
+                count++;
+                Assert.IsTrue(state.Changed.IsStateChanged());
+            });
+
+            Assert.IsFalse(store.State.Changed.IsStateChanged());
+            store.State.Changed.SetStateChanged();
+
+            store.ForceUpdate();
+            Assert.IsFalse(store.State.Changed.IsStateChanged());
+            Assert.AreEqual(1, count);
+        }
+
+        class State : StateBase<State>
+        {
+            public ChangedState Changed { get; set; }
+
+            public State()
+            {
+                this.Changed = new ChangedState();
+            }
+
+            public override State Clone()
+            {
+                var state = (State) MemberwiseClone();
+                state.Changed = state.Changed.Clone();
+                return state;
+            }
+        }
+
+        class ChangedState : StateElement<ChangedState>
+        {
+        }
+
+        class Action
+        {
+        }
+
+        class SampleRender
+        {
+            public int Count = 0;
+
+            public void Render(State state)
+            {
+                Count++;
+            }
+        }
+
+        class SampleReducer
+        {
+            public int Count = 0;
+
+            public State Reduce(State state, Action action)
+            {
+                Count++;
+                return state;
+            }
         }
     }
 }
