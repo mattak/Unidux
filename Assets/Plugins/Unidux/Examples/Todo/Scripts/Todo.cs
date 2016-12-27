adc3b0221f314c693c741660d8bf8b0fed490e2e
mattak <mattak.me@gmail.com>
2016-12-27 23:48:45 +0900

    feat(Example/Todo): shortcut created

--- Assets/Plugins/Unidux/Examples/Todo/Scripts/Todo.cs
+++ Assets/Plugins/Unidux/Examples/Todo/Scripts/Todo.cs
@@ -5,5 +5,12 @@
         public uint Id;
         public string Text;
         public bool Completed;
+
+        public Todo(uint id = 0, string text = "", bool completed = false)
+        {
+            this.Id = id;
+            this.Text = text;
+            this.Completed = completed;
+        }
     }
 }
