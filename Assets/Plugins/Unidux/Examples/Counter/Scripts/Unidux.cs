ba3669c36e6e9faff349bb30f9c260717c979226
mattak <mattak.me@gmail.com>
2017-06-15 00:55:19 +0900

    issues/47 feat(Example/Counter): support IStoreAccessor

--- Assets/Plugins/Unidux/Examples/Counter/Scripts/Unidux.cs
+++ Assets/Plugins/Unidux/Examples/Counter/Scripts/Unidux.cs
@@ -1,9 +1,13 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
+using UnityEngine;
 
 namespace Unidux.Example.Counter
 {
-    public sealed class Unidux : SingletonMonoBehaviour<Unidux>
+    public sealed class Unidux : SingletonMonoBehaviour<Unidux>, IStoreAccessor
     {
+        public TextAsset InitialStateJson;
+
         private Store<State> _store;
 
         public static State State
@@ -11,14 +15,40 @@ namespace Unidux.Example.Counter
             get { return Store.State; }
         }
 
+        public object StateObject
+        {
+            get { return State; }
+            set { Store.State = (State) value; }
+        }
+
+        public Type StateType
+        {
+            get { return typeof(State); }
+        }
+
         public static Subject<State> Subject
         {
             get { return Store.Subject; }
         }
 
+        private static IReducer[] Reducers
+        {
+            get { return new IReducer[] {new Count.Reducer()}; }
+        }
+
+        private static State InitialState
+        {
+            get
+            {
+                return Instance.InitialStateJson != null
+                    ? JsonUtility.FromJson<State>(Instance.InitialStateJson.text)
+                    : new State();
+            }
+        }
+
         public static Store<State> Store
         {
-            get { return Instance._store = Instance._store ?? new Store<State>(new State(), new Count.Reducer()); }
+            get { return Instance._store = Instance._store ?? new Store<State>(InitialState, Reducers); }
         }
 
         public static void Dispatch<TAction>(TAction action)
