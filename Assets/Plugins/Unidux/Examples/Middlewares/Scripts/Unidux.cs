53c59747ad39fdeaa3b26cbda5c48cc400aa6375
mattak <mattak.me@gmail.com>
2017-06-27 00:22:16 +0900

    issues/85 feat(Example): Middleware samples (thunk, logger, crashreport)

---
+++ Assets/Plugins/Unidux/Examples/Middlewares/Scripts/Unidux.cs
@@ -0,0 +1,65 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Unidux.Example.Middlewares
+{
+    public class Unidux : SingletonMonoBehaviour<Unidux>
+    {
+        public TextAsset InitialStateJson;
+
+        private Store<State> _store;
+
+        public IStoreObject StoreObject
+        {
+            get { return Store; }
+        }
+
+        public static State State
+        {
+            get { return Store.State; }
+        }
+
+        public static Subject<State> Subject
+        {
+            get { return Store.Subject; }
+        }
+
+        private static State InitialState
+        {
+            get
+            {
+                return Instance.InitialStateJson != null
+                    ? JsonUtility.FromJson<State>(Instance.InitialStateJson.text)
+                    : new State();
+            }
+        }
+
+        public static Store<State> Store
+        {
+            get
+            {
+                return Instance._store =
+                    Instance._store ?? new Store<State>(InitialState, new MiddlewareDuck.Reducer());
+            }
+        }
+
+        public static object Dispatch<TAction>(TAction action)
+        {
+            return Store.Dispatch(action);
+        }
+
+        void Start()
+        {
+            Store.ApplyMiddlewares(
+                Middlewares.Thunk,
+                Middlewares.Logger,
+                Middlewares.CrashReport
+            );
+        }
+
+        void Update()
+        {
+            Store.Update();
+        }
+    }
+}
