3f8c021fde68d1b0c91d85f984bb23fd1f28b54e
mattak <mattak.me@gmail.com>
2017-09-08 11:59:28 +0900

    issues/94 feat(Example): Scripts for trying http get request

---
+++ Assets/Plugins/Unidux/Examples/SimpleHttp/Scripts/State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Unidux.Example.SimpleHttp
+{
+    [Serializable]
+    public partial class State : StateBase
+    {
+        public string Url = "";
+        public int StatusCode = -1;
+        public string Body = "";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is State)
+            {
+                var target = (State) obj;
+                return
+                    this.Url == target.Url &&
+                    this.StatusCode == target.StatusCode &&
+                    this.Body == target.Body;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
