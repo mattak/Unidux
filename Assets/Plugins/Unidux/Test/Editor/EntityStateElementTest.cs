b3dc25d6c5b578d6c566926faba5527f9a3d8710
mattak <mattak.me@gmail.com>
2017-09-15 18:19:46 +0900

    issues/103 feat(StateElement): EntityStateElement created
    It is shortcut for representing StateElement which refering pre-existing entity.

---
+++ Assets/Plugins/Unidux/Test/Editor/EntityStateElementTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Unidux
+{
+    public class EntityStateElementTest
+    {
+        [Test]
+        public void EqualsTest()
+        {
+            var state1 = new SampleStateElement();
+            var state2 = new SampleStateElement();
+            Assert.AreEqual(state1, state2);
+
+            state1.Entity = new SampleEntity();
+            state2.Entity = new SampleEntity();
+            state1.Entity.Id = 1;
+            state2.Entity.Id = 2;
+
+            Assert.AreNotEqual(state1, state2);
+        }
+
+        [Test]
+        public void CloneTest()
+        {
+            var state1 = new SampleStateElement();
+            state1.Entity = new SampleEntity();
+
+            var state2 = (SampleStateElement) state1.Clone();
+            Assert.AreEqual(state1, state2);
+
+            state1.Entity.Id = 1;
+            state2.Entity.Id = 2;
+
+            Assert.AreNotEqual(state1, state2);
+        }
+
+        class SampleEntity : ICloneable
+        {
+            public int Id;
+
+            public override bool Equals(object obj)
+            {
+                if (obj is SampleEntity)
+                {
+                    var target = (SampleEntity) obj;
+                    return this.Id == target.Id;
+                }
+                return false;
+            }
+
+            public object Clone()
+            {
+                var target = new SampleEntity();
+                target.Id = this.Id;
+                return target;
+            }
+        }
+
+        class SampleStateElement : EntityStateElement<SampleEntity>
+        {
+        }
+    }
+}
