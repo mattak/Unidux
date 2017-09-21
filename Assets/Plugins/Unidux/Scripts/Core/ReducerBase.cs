39733811c080b14ece4c4f4a582bee9426e8f47f
mattak <mattak.me@gmail.com>
2017-09-22 00:16:53 +0900

    issues/15 refactor(ReducerBase): to be virtual ReduceAny, IsMatchedAction

--- Assets/Plugins/Unidux/Scripts/Core/ReducerBase.cs
+++ Assets/Plugins/Unidux/Scripts/Core/ReducerBase.cs
@@ -4,12 +4,12 @@ namespace Unidux
     {
         public abstract TState Reduce(TState state, TAction action);
 
-        public object ReduceAny(object state, object action)
+        public virtual object ReduceAny(object state, object action)
         {
             return this.Reduce((TState) state, (TAction) action);
         }
 
-        public bool IsMatchedAction(object action)
+        public virtual bool IsMatchedAction(object action)
         {
             return action is TAction;
         }
