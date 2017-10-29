d8f9aefb645ba072fa072982116330cd5477e458
mattak <mattak.me@gmail.com>
2017-10-30 01:22:39 +0900

    issues/118 fix(Examples/SimpleHttp): Equals GetHashCode refer StateBase

--- Assets/Plugins/Unidux/Examples/SimpleHttp/Scripts/State.cs
+++ Assets/Plugins/Unidux/Examples/SimpleHttp/Scripts/State.cs
@@ -8,24 +8,5 @@ namespace Unidux.Example.SimpleHttp
         public string Url = "";
         public int StatusCode = -1;
         public string Body = "";
-
-        public override bool Equals(object obj)
-        {
-            if (obj is State)
-            {
-                var target = (State) obj;
-                return
-                    this.Url == target.Url &&
-                    this.StatusCode == target.StatusCode &&
-                    this.Body == target.Body;
-            }
-
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
     }
 }
